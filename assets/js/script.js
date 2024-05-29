f3ae4db88db800491b5ffa265f8f3d8e749dac45
Sarah McLoughlin <boosarahs@gmail.com>
2024-05-29 18:56:07 +0000

    Add loop to audio so doesn't stop after 1 play

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 /** Audio */
+//Toggle on/off - adapted in codepen from tutorial https://stackoverflow.com/questions/55018585/how-to-turn-on-audio-on-click-icon-play-pause
 function togglePlay() {
     let audio = document.getElementsByTagName("audio")[0]; /* 0 v important here */
     if (audio.paused) {
